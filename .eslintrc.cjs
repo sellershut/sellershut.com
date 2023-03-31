55fee5e299928cd542c6b5365137d0a6997c4c96
kawaki-san <dev@kanjala.com>
2023-03-31 03:39:40 +0200

    fix: resolve  imports

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,6 +14,14 @@ module.exports = {
   settings: {
     'svelte3/typescript': () => require('typescript'),
     'import/resolver': {
+      'eslint-import-resolver-custom-alias': {
+        alias: {
+          $lib: './src/lib',
+          $app: './.svelte-kit/runtime/app',
+          '@sveltejs': './.svelte-kit/dev',
+        },
+        extensions: ['.js'],
+      },
       typescript: {
         alwaystrytypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
       },
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script>
+  import Navbar from '$lib/navbar.svelte';
   import '../global.css';
 </script>
 
 <main>
+  <Navbar />
   <slot />
 </main>
