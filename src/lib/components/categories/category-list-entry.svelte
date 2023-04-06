245087be8c778e0637da6b3d91f9256df23a86d2
kawaki-san <dev@kanjala.com>
2023-04-06 15:17:23 +0200

    add: types to category component

---
+++ src/lib/components/categories/category-list-entry.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import type { ICategory } from '$lib/types/category';
+  import type { SvelteComponent } from 'svelte';
+
+  export let focused = false;
+  export let icons: Map<string, typeof SvelteComponent>;
+  export let category: ICategory;
+</script>
+
+<a
+  href={'#'}
+  class={`flex flex-col items-center gap-2 p-3 ${
+    focused ? 'border-b-2' : ''
+  } text-zinc-600 dark:text-zinc-400 hover:text-zinc-800 dark:hover:text-zinc-200 transition cursor-pointer hover:border-b-2 border-rose-500`}
+>
+  <svelte:component this={icons.get(category.name)} />
+  <p class="font-medium text-center text-xs small-caps">
+    {category.name}
+  </p>
+</a>
--- src/lib/components/categories/category-list.svelte
+++ src/lib/components/categories/category-list.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import type { ICategory } from '$lib/types/category';
+  import type { SvelteComponent } from 'svelte';
   import IconArt from '../icons/icon-art.svelte';
   import IconBaby from '../icons/icon-baby.svelte';
   import IconBeauty from '../icons/icon-beauty.svelte';
@@ -12,65 +13,61 @@
   import IconShirt from '../icons/icon-shirt.svelte';
   import IconSporting from '../icons/icon-sporting.svelte';
   import IconToy from '../icons/icon-toy.svelte';
+  import CategoryListEntry from './category-list-entry.svelte';
 
   export let categories: ICategory[];
 
-  const hashMap = new Map();
+  const sort = (arr: ICategory[]): ICategory[] => arr.sort((a, b) => a.name.localeCompare(b.name));
+
+  const icons = new Map<string, typeof SvelteComponent>();
 
   for (let index = 0; index < categories.length; index += 1) {
     const category = categories[index];
     switch (category.name.toLowerCase()) {
       case 'electronics':
-        hashMap.set(category.name, IconElectronics);
+        icons.set(category.name, IconElectronics);
         break;
       case 'apparel':
-        hashMap.set(category.name, IconShirt);
+        icons.set(category.name, IconShirt);
         break;
       case 'pets':
-        hashMap.set(category.name, IconPet);
+        icons.set(category.name, IconPet);
         break;
       case 'garden & home':
-        hashMap.set(category.name, IconGardenHome);
+        icons.set(category.name, IconGardenHome);
         break;
       case 'baby supplies':
-        hashMap.set(category.name, IconBaby);
+        icons.set(category.name, IconBaby);
         break;
       case 'collectibles & art':
-        hashMap.set(category.name, IconArt);
+        icons.set(category.name, IconArt);
         break;
       case 'property':
-        hashMap.set(category.name, IconHome);
+        icons.set(category.name, IconHome);
         break;
       case 'toys & recreational':
-        hashMap.set(category.name, IconToy);
+        icons.set(category.name, IconToy);
         break;
       case 'vehicles':
-        hashMap.set(category.name, IconCar);
+        icons.set(category.name, IconCar);
         break;
       case 'health & beauty':
-        hashMap.set(category.name, IconBeauty);
+        icons.set(category.name, IconBeauty);
         break;
       case 'books, movies & music':
-        hashMap.set(category.name, IconLibrary);
+        icons.set(category.name, IconLibrary);
         break;
       case 'sporting':
-        hashMap.set(category.name, IconSporting);
+        icons.set(category.name, IconSporting);
         break;
       default:
-        hashMap.set(category.name, IconHome);
+        icons.set(category.name, IconHome);
     }
   }
 </script>
 
 <div class="pt-4 flex items-stretch justify-between overflow-x-auto">
-  {#each categories as category}
-    <div
-      class="flex flex-col items-center gap-2 p-3 border-b-2 hover:text-zinc-800 transition cursor-pointer"
-    >
-      <svelte:component this={hashMap.get(category.name)} />
-      <p class="font-medium text-center text-xs">
-        {category.name}
-      </p>
-    </div>
+  {#each sort(categories) as category}
+    <CategoryListEntry {icons} {category} />
   {/each}
 </div>
