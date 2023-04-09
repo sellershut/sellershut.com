cb1e216435ca49146778407cec1beb39b5645e16
kawaki-san <dev@kanjala.com>
2023-04-09 22:49:40 +0200

    feat: populate nav

--- src/lib/components/navigation/navigation-bar.svelte
+++ src/lib/components/navigation/navigation-bar.svelte
@@ -2,7 +2,9 @@
   import { createQuery } from '@tanstack/svelte-query';
   import { findCategories, keyRootCategories } from '$lib/api/category/query';
   import type { CategoriesResult } from '$lib/@types/category';
-  import { fly } from 'svelte/transition';
+  import { fly, scale } from 'svelte/transition';
+  import { quintOut } from 'svelte/easing';
+  import IconHome from '../icons/icon-home.svelte';
 
   const categories = createQuery<CategoriesResult, Error>({
     queryKey: [keyRootCategories, 7],
@@ -10,9 +12,11 @@
   });
   let openMobileNav = false;
   const searchFocused = false;
+  const showSearchContainer = false;
   const toggleMobileNav = () => {
     openMobileNav = !openMobileNav;
   };
+  const scaleFactor = 300;
 </script>
 
 <!-- 
@@ -43,6 +47,7 @@ md:mx-auto px-2 fixed top-0 left-0 right-0 h-10 items-center"
       <li>
         <a
           href={'#'}
+          on:click={toggleMobileNav}
           class={`h-10 w-5 ${
             openMobileNav ? '' : 'aspect-square'
           } flex items-center cursor-pointer`}
@@ -63,6 +68,42 @@ md:mx-auto px-2 fixed top-0 left-0 right-0 h-10 items-center"
           </div>
         </a>
       </li>
+      {#if !showSearchContainer}
+        <li
+          in:scale={{ duration: 0.5 * scaleFactor, easing: quintOut }}
+          class="w-1/3 md:w-auto text-center md:text-left"
+        >
+          <a href="/"><IconHome class="mx-auto" /></a>
+        </li>
+        {#if $categories.isLoading}
+          <span>Loading</span>
+        {:else if $categories.isError}
+          <span>Error: {$categories.error.message}</span>
+        {:else}
+          <li
+            in:scale={{ duration: scaleFactor, easing: quintOut }}
+            class="hidden md:flex h-full items-center justify-center"
+          >
+            <a class="mx-4 py-3 opacity-80 hover:opacity-100 inline" href={'/'}
+              >All</a
+            >
+          </li>
+          {#each $categories.data.categories as category, i}
+            <li
+              in:scale={{
+                duration: ((3 + i) / 2) * scaleFactor,
+                easing: quintOut,
+              }}
+              class="hidden md:flex h-full items-center justify-center"
+            >
+              <a
+                class="mx-4 py-3 opacity-100 hover:opacity-100 inline"
+                href={'#'}>{category.name}</a
+              >
+            </li>
+          {/each}
+        {/if}
+      {/if}
     </ul>
   {/if}
 </nav>
