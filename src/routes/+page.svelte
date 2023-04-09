1725cd0d7b43a1ccfbd60e9c6e3bbe67fabc0273
kawaki-san <dev@kanjala.com>
2023-04-10 00:47:13 +0200

    feat: immediately load theme from cookie

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<h1 class="text-9xl">Welcome to SvelteKit</h1>
+<h1 class="text-9xl text-blue-950 dark:text-red-950">Welcome to SvelteKit</h1>
 <p>
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
