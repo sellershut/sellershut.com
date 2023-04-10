b0e711646714a35f298a1c2b648421330f2b2b3a
kawaki-san <dev@kanjala.com>
2023-04-10 18:45:00 +0200

    list categories

--- src/lib/components/hero/hero.svelte
+++ src/lib/components/hero/hero.svelte
@@ -17,4 +17,24 @@
   });
 </script>
 
-<div>Top Categories</div>
+<div class="w-full bg-zinc-500 p-4 rounded-md flex flex-col">
+  <a href={'#'}>View All</a>
+  {#if $categories.isLoading}
+    <span>Loading</span>
+  {:else if $categories.isError}
+    <span>{$categories.error.message}</span>
+  {:else if $categories.isSuccess}
+    <div class="flex flex-col gap-4 items-center justify-center">
+      {#each $categories.data.categories as category}
+        <div class="bg-red-500 mx-auto">
+          <img
+            class="h-48 rounded-full aspect-square bg-green-500"
+            alt={`${category.name} image`}
+            src={category.imageUrl}
+          />
+          <p class="small-caps text-center">{category.name}</p>
+        </div>
+      {/each}
+    </div>
+  {/if}
+</div>
