db8d926747944787f0238dbcab2bec6da04cf199
kawaki-san <dev@kanjala.com>
2023-04-12 01:34:48 +0200

    [query]: find user by account

--- src/lib/api/database-adapter.ts
+++ src/lib/api/database-adapter.ts
@@ -1,58 +1,62 @@
 import type {
-    Adapter,
-    AdapterAccount,
-    AdapterSession,
-    AdapterUser,
+  Adapter,
+  AdapterAccount,
+  AdapterSession,
+  AdapterUser,
 } from '@auth/core/adapters';
 import type { Awaitable } from '@auth/core/types';
 import { apiCreateUser, apiUpdateUser } from './user/mutation';
-import { apiGetUser, apiGetUserByEmail } from './user/query';
+import {
+  apiGetUser,
+  apiGetUserByAccount,
+  apiGetUserByEmail,
+} from './user/query';
 
 export const DatabaseAdapter: Adapter = {
-    createUser(user: Omit<AdapterUser, 'id'>): Awaitable<AdapterUser> {
-        return apiCreateUser(user);
-    },
-    getUser(id: string): Awaitable<AdapterUser | null> {
-        return apiGetUser(id);
-    },
-    getUserByEmail(email: string): Awaitable<AdapterUser | null> {
-        return apiGetUserByEmail(email);
-    },
-    getUserByAccount(
-        providerAccountId: Pick<AdapterAccount, 'provider' | 'providerAccountId'>,
-    ): Awaitable<AdapterUser | null> {
-        throw new Error('Function not implemented.');
-    },
-    updateUser(user: Partial<AdapterUser>): Awaitable<AdapterUser> {
-        return apiUpdateUser(user);
-    },
-    linkAccount(
-        account: AdapterAccount,
-    ): Promise<void> | Awaitable<AdapterAccount | null | undefined> {
-        throw new Error('Function not implemented.');
-    },
-    createSession(session: {
-        sessionToken: string;
-        userId: string;
-        expires: Date;
-    }): Awaitable<AdapterSession> {
-        throw new Error('Function not implemented.');
-    },
-    getSessionAndUser(
-        sessionToken: string,
-    ): Awaitable<{ session: AdapterSession; user: AdapterUser } | null> {
-        throw new Error('Function not implemented.');
-    },
-    updateSession(
-        session: Partial<AdapterSession> & Pick<AdapterSession, 'sessionToken'>,
-    ): Awaitable<AdapterSession | null | undefined> {
-        throw new Error('Function not implemented.');
-    },
-    deleteSession(
-        sessionToken: string,
-    ): Promise<void> | Awaitable<AdapterSession | null | undefined> {
-        throw new Error('Function not implemented.');
-    },
+  createUser(user: Omit<AdapterUser, 'id'>): Awaitable<AdapterUser> {
+    return apiCreateUser(user);
+  },
+  getUser(id: string): Awaitable<AdapterUser | null> {
+    return apiGetUser(id);
+  },
+  getUserByEmail(email: string): Awaitable<AdapterUser | null> {
+    return apiGetUserByEmail(email);
+  },
+  getUserByAccount(
+    providerAccountId: Pick<AdapterAccount, 'provider' | 'providerAccountId'>,
+  ): Awaitable<AdapterUser | null> {
+    return apiGetUserByAccount(providerAccountId);
+  },
+  updateUser(user: Partial<AdapterUser>): Awaitable<AdapterUser> {
+    return apiUpdateUser(user);
+  },
+  linkAccount(
+    account: AdapterAccount,
+  ): Promise<void> | Awaitable<AdapterAccount | null | undefined> {
+    throw new Error('Function not implemented.');
+  },
+  createSession(session: {
+    sessionToken: string;
+    userId: string;
+    expires: Date;
+  }): Awaitable<AdapterSession> {
+    throw new Error('Function not implemented.');
+  },
+  getSessionAndUser(
+    sessionToken: string,
+  ): Awaitable<{ session: AdapterSession; user: AdapterUser } | null> {
+    throw new Error('Function not implemented.');
+  },
+  updateSession(
+    session: Partial<AdapterSession> & Pick<AdapterSession, 'sessionToken'>,
+  ): Awaitable<AdapterSession | null | undefined> {
+    throw new Error('Function not implemented.');
+  },
+  deleteSession(
+    sessionToken: string,
+  ): Promise<void> | Awaitable<AdapterSession | null | undefined> {
+    throw new Error('Function not implemented.');
+  },
 };
 
 export default DatabaseAdapter;
--- src/lib/api/user/query.ts
+++ src/lib/api/user/query.ts
@@ -1,4 +1,4 @@
-import type { AdapterUser } from '@auth/core/adapters';
+import type { AdapterAccount, AdapterUser } from '@auth/core/adapters';
 import axios from 'axios';
 import { PUBLIC_API_ENDPOINT } from '$env/static/public';
 
@@ -6,7 +6,7 @@ export const apiGetUser = async (id: string): Promise<AdapterUser | null> => {
   const data = await axios.post(
     PUBLIC_API_ENDPOINT,
     {
-      query: `query getUser($id: String) {
+      query: `query getUser($id: String!) {
                 getUser(id: $id){
                     id
                     name
@@ -34,7 +34,7 @@ export const apiGetUserByEmail = async (
   const data = await axios.post(
     PUBLIC_API_ENDPOINT,
     {
-      query: `query getUserByEmail($email: String) {
+      query: `query getUserByEmail($email: String!) {
                 getUserByEmail(email: $email){
                     id
                     name
@@ -54,6 +54,32 @@ export const apiGetUserByEmail = async (
     },
   );
   return data.data.data.getUserByEmail;
+};
+
+export const apiGetUserByAccount = async (
+  providerAccountId: Pick<AdapterAccount, 'provider' | 'providerAccountId'>,
+): Promise<AdapterUser | null> => {
+  const data = await axios.post(
+    PUBLIC_API_ENDPOINT,
+    {
+      query: `query getUserByAccount($provider: String!, $providerAccountId: String!) {
+                getUserByAccount(providerAccountId: $providerAccountId, provider: $provider){
+                    id
+                    name
+                    image
+                    emailVerified
+                    email
+                }
+            }`,
+      variables: providerAccountId,
+    },
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    },
+  );
+  return data.data.data.getUserByAccount;
 };
 
 export default apiGetUser;
