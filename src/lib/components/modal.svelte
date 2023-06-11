4ae2624ca03f6396f3bec631525168c01a5a132e
kawaki-san <dev@kanjala.com>
2023-06-11 20:42:23 +0200

    add: next button to create ad modal

--- src/lib/components/modal.svelte
+++ src/lib/components/modal.svelte
@@ -9,7 +9,7 @@
   class="top-0 left-0 text-zinc-700 dark:text-zinc-200 absolute w-full h-full flex justify-center items-center bg-black/40 z-50 cursor-default"
 >
   <div
-    class="fixed h-5/6 md:h-4/6 lg:h-1/2 p-4 bg-zinc-50/80 dark:bg-zinc-900/80 backdrop-blur-xl w-10/12 md:w-8/12 lg:w-6/12 rounded shadow drop-shadow-sm"
+    class="fixed max-h-96 h-full p-4 bg-zinc-50/80 dark:bg-zinc-900/80 backdrop-blur-xl w-10/12 md:w-8/12 lg:w-6/12 rounded shadow drop-shadow-sm"
   >
     <svelte:component this={modalContent} on:click />
   </div>
--- src/lib/components/modal/create-ad.svelte
+++ src/lib/components/modal/create-ad.svelte
@@ -5,6 +5,7 @@
   import { byTextAscending } from '$lib/util/sort/sorter';
   import { categoryIcons } from '$lib/@types/category-icons';
   import CreateAdCategoryElement from './create-ad/create-ad-category-element.svelte';
+  import IconBackCircle from '../icons/icon-back-circle.svelte';
 
   let parentId = 0;
   $: categories = createQuery<CategoriesResult, Error>({
@@ -15,7 +16,11 @@
   $: selectedCategories = [];
 </script>
 
-<div class="flex flex-col justify-between gap-4 h-full">
+<div
+  class={`flex flex-col ${
+    selectedCategories.length ? 'justify-between' : ''
+  } gap-4 h-full`}
+>
   <div>
     <h3 class="text-xl font-medium title-font">Select Category</h3>
     <p class="text-sm">What best describes the item you want to sell?</p>
@@ -42,12 +47,25 @@
     {/if}
   </div>
   {#if selectedCategories.length}
-    <button
-      on:click={() => {
-        const lastItem = selectedCategories.pop();
-        console.log(lastItem);
-        parentId = lastItem ? lastItem.parentId : 0;
-      }}>Back</button
-    >
+    <div class="flex justify-between px-4">
+      <button
+        class="inline-flex hover:text-rose-500 duration-200"
+        on:click={() => {
+          const lastItem = selectedCategories.pop();
+          parentId = lastItem ? lastItem.parentId : 0;
+        }}
+      >
+        <IconBackCircle />
+        Back</button
+      >
+      <button
+        class="inline-flex hover:text-rose-500 duration-200"
+        on:click={() => {
+          // fill in ad info
+        }}
+        >Looks Good
+        <IconBackCircle class="rotate-180" />
+      </button>
+    </div>
   {/if}
 </div>
