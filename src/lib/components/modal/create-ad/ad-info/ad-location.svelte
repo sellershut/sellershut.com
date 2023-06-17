2f77d67ecd1d418e125828d6fd8a5636ef52e2af
kawaki-san <dev@kanjala.com>
2023-06-17 15:19:30 +0200

    style: theme select input

--- src/lib/components/modal/create-ad/ad-info/ad-location.svelte
+++ src/lib/components/modal/create-ad/ad-info/ad-location.svelte
@@ -1,13 +1,29 @@
 <script lang="ts">
   import type { Country } from '$lib/@types/country';
+  import { rose400, rose500, zinc300, zinc800 } from '$lib/data/colours';
   import { formattedCountries, getCountryByValue } from '$lib/data/countries';
+  import { onMount } from 'svelte';
   import Select from 'svelte-select';
 
   let value: Country;
   $: icon = getCountryByValue(value?.value)?.icon;
+
+  let isDark: boolean;
+  onMount(() => {
+    isDark = document.documentElement.classList.contains('dark');
+  });
 </script>
 
-<Select items={formattedCountries} bind:value>
+<Select
+  --border={`2px solid ${rose400}`}
+  --border-hover={`2px solid ${rose500}`}
+  --border-focused={`2px solid ${rose500}`}
+  --background={isDark ? { zinc800 } : { zinc300 }}
+  --list-background={isDark ? { zinc800 } : { zinc300 }}
+  --item-hover-bg={`${rose500}`}
+  items={formattedCountries}
+  bind:value
+>
   <div slot="prepend">
     {#if icon}
       <div class="pr-2">{icon}</div>
---
+++ src/lib/data/colours.ts
@@ -0,0 +1,4 @@
+export const rose400 = '#fb7185';
+export const rose500 = '#f43f5e';
+export const zinc800 = '#27272a';
+export const zinc300 = '#d4d4d8';
