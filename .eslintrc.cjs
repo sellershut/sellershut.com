b2a8f93cf04c80720c745f004fab40cf37ae5c46
rtkay123 <dev@kanjala.com>
2024-01-29 15:36:30 +0200

    add: lint cfg

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,31 +1,63 @@
 /** @type { import("eslint").Linter.Config } */
 module.exports = {
-	root: true,
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:svelte/recommended',
-		'prettier'
-	],
-	parser: '@typescript-eslint/parser',
-	plugins: ['@typescript-eslint'],
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2020,
-		extraFileExtensions: ['.svelte']
-	},
-	env: {
-		browser: true,
-		es2017: true,
-		node: true
-	},
-	overrides: [
-		{
-			files: ['*.svelte'],
-			parser: 'svelte-eslint-parser',
-			parserOptions: {
-				parser: '@typescript-eslint/parser'
-			}
-		}
-	]
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:svelte/recommended',
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  ignorePatterns: ['/*.cjs', '/*.js', '/*.ts'],
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020,
+    extraFileExtensions: ['.svelte'],
+    project: './tsconfig.json',
+  },
+  env: {
+    browser: true,
+    es2017: true,
+    node: true,
+  },
+  settings: {
+    'import/resolver': {
+      'eslint-import-resolver-custom-alias': {
+        alias: {
+          $app: './node_modules/@sveltejs/kit/src/runtime/app',
+          $lib: './src/lib',
+        },
+        extensions: ['.ts', '.js'],
+      },
+      typescript: {
+        alwaystrytypes: true,
+      },
+    },
+  },
+  overrides: [
+    {
+      files: ['*.svelte'],
+      parser: 'svelte-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+      },
+      rules: {
+        'import/no-mutable-exports': 0,
+        'import/prefer-default-export': 0,
+      },
+    },
+    {
+      files: ['./src/routes/**/*.ts'],
+      rules: {
+        'import/prefer-default-export': 0,
+      },
+    },
+  ],
+  rules: {
+    'import/no-extraneous-dependencies': 0,
+    'import/extensions': 0,
+  },
 };
--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,8 +1,8 @@
 module.exports = {
-	"useTabs": true,
-	"singleQuote": true,
-	"trailingComma": "none",
-	"printWidth": 100,
-	"plugins": ["prettier-plugin-svelte"],
-	"overrides": [{ "files": "*.svelte", "options": { "parser": "svelte" } }]
-}
+  useTabs: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  printWidth: 100,
+  plugins: ['prettier-plugin-svelte'],
+  overrides: [{ files: '*.svelte', options: { parser: 'svelte' } }],
+};
