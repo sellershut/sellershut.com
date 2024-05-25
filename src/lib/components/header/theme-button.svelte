1ee1b3c4dc33e186306a16700492e2dd13b94452
rtkay123 <dev@kanjala.com>
2024-05-25 17:04:28 +0200

    add: icons to dropdown

--- src/lib/components/header/theme-button.svelte
+++ src/lib/components/header/theme-button.svelte
@@ -3,6 +3,12 @@ import { enhance } from "$app/forms";
 import { page } from "$app/stores";
 import * as DropdownMenu from "$lib/components/ui/dropdown-menu";
 import type { SubmitFunction } from "@sveltejs/kit";
+import {
+	MoonOutline,
+	PhonePortraitOutline,
+	SunnyOutline,
+	TvOutline,
+} from "svelte-ionicons";
 
 const submitUpdateTheme: SubmitFunction = ({ action }) => {
 	const theme = action.searchParams.get("theme");
@@ -23,13 +29,22 @@ const createRoute = (theme: string): string => {
     <DropdownMenu.Group>
       <form use:enhance={submitUpdateTheme} method="post">
         <DropdownMenu.Item>
-          <button formaction={createRoute("dark")}>Dark</button>
+          <button formaction={createRoute("dark")} class="inline-flex gap-2"
+            ><MoonOutline /> Dark</button
+          >
         </DropdownMenu.Item>
         <DropdownMenu.Item>
-          <button formaction={createRoute("light")}>Light</button>
+          <button formaction={createRoute("light")} class="inline-flex gap-2"
+            ><SunnyOutline />Light</button
+          >
         </DropdownMenu.Item>
         <DropdownMenu.Item>
-          <button formaction={createRoute("system")}>System</button>
+          <button formaction={createRoute("system")} class="inline-flex gap-2"
+            >
+            <TvOutline class="hidden md:block"/>
+            <PhonePortraitOutline class="md:hidden"/>
+            System</button
+          >
         </DropdownMenu.Item>
       </form>
     </DropdownMenu.Group>
